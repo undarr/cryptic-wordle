1168dc7e447defa3a147ee663a6263aca3a5069f
Ulfred Chan <101036368+undarr@users.noreply.github.com>
2026-08-10 23:07:39 +0800

    Fix console log syntax error in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -186,7 +186,7 @@ function App() {
     settomorrow(nextday);
     const tdy = Date.now();
     const startOfToday = Math.floor((tdy-eighttotwelve) / (24 * 60 * 60 * 1000)) * (24 * 60 * 60 * 1000)+eighttotwelve;
-    console.log(console.log(new Date(startOfToday).toISOString()); 
+    console.log(new Date(startOfToday).toISOString()); 
     const url =
       'https://api.browse.ai/v2/robots/ef597c3b-e228-4444-952d-6de2a65681c7/tasks';
     const headers = {
